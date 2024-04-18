88a2143dc4ac44962be50d128e64b077e5ad5f94
XinxiangZ <100697002+XinxiangZ@users.noreply.github.com>
2024-04-18 13:57:01 +0200

    1

--- Backend/app.py
+++ Backend/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/', methods=['POST', 'OPTIONS'])
 
 def procesar_datos():
-    # Si la solicitud es OPTIONS, responde con los encabezados necesarios
+
     if request.method == 'OPTIONS':
         response = jsonify({'message': 'Preflight Request Handled'})
         response.headers.add('Access-Control-Allow-Origin', '*')
@@ -13,16 +13,14 @@ def procesar_datos():
         response.headers.add('Access-Control-Allow-Methods', 'POST, OPTIONS')
         return response
     else:
-        # Si la solicitud es POST, procesa los datos recibidos
+
         data = request.json
         text_input = data.get('textInput')
         selected_option = data.get('selectedOption')
        
         respuesta = {'textInput': text_input, 'selectedOption': selected_option}
         print(respuesta)
-        # Realiza cualquier procesamiento necesario aquí
-        
-        # Devuelve la respuesta correspondiente
+
         return jsonify(respuesta)
 
 if __name__ == '__main__':
