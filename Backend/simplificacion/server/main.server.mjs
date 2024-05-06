63711c76216af9e6e87800b6b2abe438577bbf70
XinxiangZ <100697002+XinxiangZ@users.noreply.github.com>
2024-05-06 13:39:59 +0200

    1

---
+++ Backend/simplificacion/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-GW2XGWU5.mjs";import"./chunk-FHN3NJQL.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- Backend/templates/simplificacion/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-OJFJC2NS.mjs";import"./chunk-BREU3PV4.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
